4628735eb33ebb2cbc5be98f39467d779ca386bd
nepom <mepomnyashchikh.a@mail.ru>
2023-05-30 18:17:22 +0700

    основной код, со всеми изменениями

--- utils/function.py
+++ utils/function.py
@@ -1,28 +1,23 @@
 import json
-import os.path
 from datetime import datetime
 
-from pip._internal import operations
 
-all_operations = os.path.join('../operations.json')
-
-
-def load_operations(filename):
+def load_operations(file_path='operations.json'):
     """
-    Вывод всех операций
+    выгружает json файл
     """
-    with open(filename, 'r', encoding='utf-8') as file:
-        operations = json.load(file)
-    return operations
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
 
 
 def get_last(operations):
     """
-    выбирает из словаря 5 операций EXECUTED
+    выбирает из словаря 5 операций  по 'EXECUTED'
     """
     last_operation = []
     for i in operations[::-1]:
-        if i["state"] == "EXECUTED":
+        if i['state'] == 'EXECUTED':
             last_operation.append(i)
             if len(last_operation) == 5:
                 break
@@ -30,30 +25,32 @@ def get_last(operations):
 
 
 def sorting_operations(operations):
+    """
+    сортирует по 'date'
+    """
     for i in range(len(operations) - 1):
         for j in range(len(operations) - i - 1):
-            if operations[j]["data"] < operations[j + 1]["data"]:
+            if operations[j]['date'] < operations[j + 1]['date']:
                 operations[j], operations[j + 1] = operations[j + 1], operations[j]
     return operations
 
 
-def card_number_masking(operations):
-    number = operations.split()[-1]
+def card_number_masking(operation):
+    """
+    маскирует номер
+    """
+    number = operation.split()[-1]
     if len(number) == 16:
-        number_masking = number[:4] + " " + number[4:6] + "** ****" + number[-4:]
-        return number_masking
-
+        musk_numb = number[:4] + " " + number[4:6] + "** ****" + number[-4:]
+        return ' '.join(operation.split()[:-1]) + ' ' + musk_numb
     else:
-        masks_number = "**" + number[-4:]
-        return masks_number
+        musk_numb = "**" + number[-4:]
+        return ' '.join(operation.split()[:-1]) + ' ' + musk_numb
 
 
 def change_date(self):
-    """Метод изменяет дату на нужную"""
+    """
+    изменяет дату в нужном формате
+    """
     date_from_str = datetime.strptime(self, '%Y-%m-%dT%H:%M:%S.%f')
     return date_from_str.strftime('%d.%m.%Y')
-
-
-if __name__ == '__main__':
-    print(load_operations(all_operations))
-    print(get_last(operations))
